4f54c1f0718067ef0316cf65d8022bce57ccad6c
baster <pnedoshivin@gmail.com>
2022-12-29 20:35:30 +0300

    Lesson_7

---
+++ lesson7/src/main/java/ru/baster/spring/shop/config/FakerConfig.java
@@ -0,0 +1,14 @@
+package ru.baster.spring.shop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.github.javafaker.Faker;
+
+@Configuration
+public class FakerConfig {
+
+    @Bean
+    public Faker faker() {
+        return new Faker();
+    }
+}
