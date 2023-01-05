5b974e44a4f0ace2b02a9e561e77b656898c9b80
baster <pnedoshivin@gmail.com>
2023-01-05 10:30:48 +0300

    Lesson_9

---
+++ lesson9/src/main/java/ru/baster/spring/shop/SpringShopApplication.java
@@ -0,0 +1,11 @@
+package ru.baster.spring.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShopApplication.class, args);
+    }
+}
